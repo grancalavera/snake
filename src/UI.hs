89922007d5af47cee9d8f6607794a16b871e0c6c
Leon Coto <leoncoto@gmail.com>
2017-10-01 20:56:09 +0100

    Complete game transcription

--- src/UI.hs
+++ src/UI.hs
@@ -79,13 +79,63 @@ handleEvent g (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt g
 handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
 handleEvent g _                                     = continue g
 
-
-
 -- Drawing
 
 drawUI :: Game -> [Widget Name]
-drawUI = undefined
+drawUI g = [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g ]
+
+drawStats :: Game -> Widget Name
+drawStats g = hLimit 11
+  $ vBox [ drawScore (g ^. score)
+         , padTop (Pad 2) $ drawGameOver (g ^. dead)
+         ]
+
+drawScore :: Int -> Widget Name
+drawScore n = withBorderStyle BS.unicodeBold
+  $ B.borderWithLabel (str "Score")
+  $ C.hCenter
+  $ padAll 1
+  $ str $ show n
+
+drawGameOver :: Bool -> Widget Name
+drawGameOver dead =
+  if dead
+    then withAttr gameOverAttr $ C.hCenter $ str "GAME OVER"
+    else emptyWidget
+
+gameOverAttr :: AttrName
+gameOverAttr = "gameOver"
+
+drawGrid :: Game -> Widget Name
+drawGrid g = withBorderStyle BS.unicodeBold
+  $ B.borderWithLabel (str "Snake")
+  $ vBox rows
+  where
+    rows         = [hBox $ cellsInRow r | r <- [height-1, height-2..0]]
+    cellsInRow y = [drawCoord (V2 x y) | x <- [0..width-1]]
+    drawCoord    = drawCell . cellAt
+    cellAt c
+      | c `elem` g ^. snake = Snake
+      | c == g ^. food      = Food
+      | otherwise           = Empty
+
+drawCell :: Cell -> Widget Name
+drawCell Snake = withAttr snakeAttr cw
+drawCell Food  = withAttr foodAttr cw
+drawCell Empty = withAttr emptyAttr cw
+
+cw :: Widget Name
+cw = str "  "
+
+snakeAttr, foodAttr, emptyAttr :: AttrName
+snakeAttr = "snakeAttr"
+foodAttr = "foodAttr"
+emptyAttr = "emptyAttr"
 
 theMap :: AttrMap
-theMap = undefined
+theMap = attrMap V.defAttr
+  [ (snakeAttr, V.blue `on` V.blue)
+  , (foodAttr, V.red `on` V.red)
+  , (gameOverAttr, fg V.red `V.withStyle` V.bold)
+  ]
 
