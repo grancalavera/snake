7b847fe3ca2543df2ea8400dda1919b37fe232f8
Leon Coto <leoncoto@gmail.com>
2017-10-04 12:29:54 +0100

    Add a record set to test stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,33 @@
-module Main (main) where
-import UI (main)
+-- |  With FlexibleContexts enabled
+--    you can have any type inside a typeclass.
+
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
+
+import Control.Monad (guard)
+import Lens.Micro.TH (makeLenses)
+import Lens.Micro ((&), (.~), (%~), (^.))
+
+-- | &    reverse application operator
+-- | .~   set
+-- | %~   over
+-- | ^.   view
+
+main = do
+  putStrLn "Nothing"
+
+data Person = Person
+  { _name :: String
+  , _email :: String
+  , _address :: [String]
+  } deriving (Show)
+
+makeLenses ''Person
+
+arturo = Person
+  { _name     = "Arturo Pex"
+  , _email    = "arturopex@gmail.com"
+  , _address  = [ "1 Random Road"
+                , "Suit 7"
+                ]
+  }
